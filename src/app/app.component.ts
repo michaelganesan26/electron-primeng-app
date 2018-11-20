107770c1833b239a8d1b16039f1e64a67a53b656
Ganesan, Michael (LRC) <michael.ganesan@lrc.ky.gov>
2018-11-20 10:54:41 -0500

    Added code for electron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'electron-primeng-app';
+  title = 'Electron PrimeNG Sample Application';
 }
